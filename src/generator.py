159a8830f237e275ba2bf96369e38fbe9ccc436d
curoky <cccuroky@gmail.com>
2021-03-27 13:03:28 +0800

    [src]: merge receipt files

--- src/generator.py
+++ src/generator.py
@@ -28,6 +28,8 @@ class FormulaContext:
     with_autotools: bool = False
     with_cmake: bool = False
 
+    bins: List[str] = None
+
     def __post_init__(self):
         if not self.tag:
             self.tag = self.version
@@ -42,28 +44,6 @@ class FormulaContext:
 
 
 
-class FormulaGenerator(object):
-
-    def __init__(self) -> None:
-        templates_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
-
-        self.env = jinja2.Environment(
-            trim_blocks=True,
-            lstrip_blocks=True,
-            loader=jinja2.FileSystemLoader(templates_folder),
-        )
-
-        self.output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Formula"))
-
-    def write_output(self, filename, content):
-        with codecs.open(os.path.join(self.output_dir, filename), 'w', 'utf8') as f:
-            f.write(content)
-
-    def generate(self, filename, template_name, ctx):
-        template = self.env.get_template(template_name + '.j2')
-        self.write_output(filename + '.rb', template.render(ctx=ctx))
-
-
 @dataclass(init=True)
 class ActionContext:
     name: str
@@ -71,24 +51,67 @@ class ActionContext:
     bins: List[str]
 
 
-class ActionGenerator(object):
+# class FormulaGenerator(object):
 
-    def __init__(self) -> None:
+#     def __init__(self) -> None:
+#         templates_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
+
+#         self.env = jinja2.Environment(
+#             trim_blocks=True,
+#             lstrip_blocks=True,
+#             loader=jinja2.FileSystemLoader(templates_folder),
+#         )
+
+#         self.output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Formula"))
+
+#     def write_output(self, filename, content):
+#         with codecs.open(os.path.join(self.output_dir, filename), 'w', 'utf8') as f:
+#             f.write(content)
+
+#     def generate(self, filename, template_name, ctx):
+#         template = self.env.get_template(template_name + '.j2')
+#         self.write_output(filename + '.rb', template.render(ctx=ctx))
+
+
+class Generator(object):
+
+    def __init__(self, name: str, bins: List[str], fctxs: List[FormulaContext]):
+        self.name = name
+        self.fctxs = fctxs
+        self.bins = bins
+
+        for ctx in self.fctxs:
+            ctx.bins = bins
+
         templates_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
-
         self.env = jinja2.Environment(
             trim_blocks=True,
             lstrip_blocks=True,
             loader=jinja2.FileSystemLoader(templates_folder),
         )
 
-        self.output_dir = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "../.github/workflows"))
-
-    def write_output(self, filename, content):
-        with codecs.open(os.path.join(self.output_dir, filename), 'w', 'utf8') as f:
+    def write_output(self, path: str, content: str):
+        with codecs.open(path, 'w', 'utf8') as f:
             f.write(content)
 
-    def generate(self, filename, ctx):
+    def generate_formula(self):
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Formula"))
+        template = self.env.get_template(f'{self.name}.rb.j2')
+        for ctx in self.fctxs:
+            output_path = os.path.join(base_path, f"{self.name}@{ctx.version}.rb")
+            self.write_output(output_path, template.render(ctx=ctx))
+
+    def generate_action(self):
+        output_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../.github/workflows",
+                         f"build-{self.name}.yaml"))
+
         template = self.env.get_template('action.yaml.j2')
-        self.write_output(f'build-{filename}.yaml', template.render(ctx=ctx))
+        ctx = ActionContext(name=self.name,
+                            versions=[fc.version for fc in self.fctxs],
+                            bins=self.bins)
+        self.write_output(output_path, template.render(ctx=ctx))
+
+    def generate(self):
+        self.generate_formula()
+        self.generate_action()
---
+++ src/main.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+from generator import Generator, FormulaContext
+from dataclasses import dataclass
+from packaging import version
+
+
+@dataclass(init=True)
+class LlvmContext(FormulaContext):
+    extra_resource: bool = False
+    use_old_registry: bool = False
+
+    def __post_init__(self):
+        super().__post_init__()
+        if version.parse(self.tag) <= version.parse('8.0.1'):
+            self.extra_resource = True
+        if version.parse(self.tag) <= version.parse('6.0.1'):
+            self.use_old_registry = True
+
+
+@dataclass(init=True)
+class ThriftContext(FormulaContext):
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.with_cmake = True
+        if version.parse(self.version) <= version.parse('0.9.2'):
+            self.with_cmake = False
+            self.with_autotools = True
+        if version.parse(self.version) <= version.parse('0.9.1'):
+            self.test_retcode = 1
+
+
+def main():
+    g = Generator(
+        name='fbthrift',
+        bins=['thrift1'],
+        fctxs=[FormulaContext(version='2019.06.03'),
+               FormulaContext(version='2019.12.30')])
+    g.generate()
+
+    g = Generator(name='llvm',
+                  bins=['clang-format', 'clang-query', 'clang-tidy'],
+                  fctxs=[
+                      LlvmContext(version='3.9.1'),
+                      LlvmContext(version='4.0.1'),
+                      LlvmContext(version='5.0.2'),
+                      LlvmContext(version='6.0.1'),
+                      LlvmContext(version='7.1.0'),
+                      LlvmContext(version='8.0.1'),
+                      LlvmContext(version='9.0.1'),
+                      LlvmContext(version='10.0.1'),
+                      LlvmContext(version='11.0.0')
+                  ])
+    g.generate()
+
+    g = Generator(
+        name='protoc',
+        bins=['protoc'],
+        fctxs=[
+            FormulaContext(version='3.1.0'),
+            FormulaContext(version='3.2.1', test_version='3.2.0'),
+            FormulaContext(version='3.3.2'),
+            FormulaContext(version='3.4.1', test_version='3.4.0'),
+            FormulaContext(version='3.5.2', test_version='3.5.1'),
+            # FormulaContext(version='3.6'),
+            FormulaContext(version='3.7.1'),
+            FormulaContext(version='3.8.0'),
+            FormulaContext(version='3.9.2'),
+            FormulaContext(version='3.13.0.1', test_version='3.13.0'),
+            FormulaContext(version='3.14.0', test_version='3.14.0'),
+            FormulaContext(version='3.15.6', test_version='3.15.6'),
+        ])
+    g.generate()
+
+    g = Generator(
+        name='thrift',
+        bins=['thrift'],
+        fctxs=[
+            # ThriftContext(version='0.2.0', test_version='20080411-exported'),
+            ThriftContext(version='0.8.0'),
+            ThriftContext(version='0.9.0'),
+            ThriftContext(version='0.9.1'),
+            ThriftContext(version='0.9.2'),
+            ThriftContext(version='0.9.3'),
+            ThriftContext(version='0.9.3.1', test_version='0.9.3'),
+            ThriftContext(version='0.10.0'),
+            ThriftContext(version='0.11.0'),
+            ThriftContext(version='0.12.0'),
+            ThriftContext(version='0.13.0'),
+            ThriftContext(version='0.14.1'),
+        ])
+    g.generate()
+
+    g = Generator(name='xz',
+                  bins=['xz'],
+                  fctxs=[
+                      FormulaContext(version='5.2.5'),
+                      FormulaContext(version='5.2.4'),
+                      FormulaContext(version='5.2.3'),
+                      FormulaContext(version='5.2.2'),
+                      FormulaContext(version='5.2.1'),
+                      FormulaContext(version='5.2.0'),
+                      FormulaContext(version='5.0.8'),
+                      FormulaContext(version='5.0.7'),
+                      FormulaContext(version='5.0.6'),
+                      FormulaContext(version='5.0.5'),
+                      FormulaContext(version='5.0.4'),
+                      FormulaContext(version='5.0.3'),
+                      FormulaContext(version='5.0.2'),
+                      FormulaContext(version='5.0.1'),
+                      FormulaContext(version='5.0.0'),
+                  ])
+    g.generate()
+    # g = Generator(name='python3', bins=[], fctxs=[])
+    # g.generate()
+
+    return 0
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
